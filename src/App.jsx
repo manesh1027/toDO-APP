49dc0f97098e823c033deffc674bf591f8ae5d24
manesh1027 <maneshsharma2710@gmail.com>
2025-01-08 10:55:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Input from "./Components/Input";
+import Button from "./Components/Button";
+import { useState } from "react";
+import Task from "./Components/Task";
+import { appStyle, inputStyle } from "./Styles/styles";
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const addTask = (e) => {
+    e.preventDefault();
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, { text: newTask, completed: false }]);
+      setNewTask("");
+    }
+  };
+
+  const completeTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div style={appStyle}>
+      <h1>Todo List</h1>
+      <form onSubmit={addTask}>
+
+        <div>
+          <Input
+            type="text"
+            style={inputStyle}
+            placeholder="Add a task"
+            handleOnChange={(e) => setNewTask(e.target.value)}
+          />
+          <Button type="submit" text=">>" />
+        </div>
+      </form>
+      <div
+        style={{
+          gap: "1em",
+          backgroundColor: "red",
+          borderRadius: "15px",
+        }}
+      >
+        {tasks.map((task, index) => {
+          return (
+            <Task
+              key={index}
+              index={index}
+              task={task}
+              completeTask={completeTask}
+              removeTask={removeTask}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Styles/Styles.js
@@ -0,0 +1,68 @@
+export const appStyle = {
+    textAlign: "center",
+    // backgroundImage: URL(""),
+    backgroundImage: "url('../assets/img.jpg')" ,
+    fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
+    padding: "20px",
+    // borderRadius: "10px",
+    height: "100vh",
+    // margin:"0",
+  };
+  
+  export const inputStyle = {
+    padding: "0.1rem 1em",
+    marginTop: "2em",
+    borderRadius: "20px",
+    border: "none",
+    fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
+    fontSize: "15px",
+    fontWeight: "bold",
+    height: "35px",
+    width: "60%",
+    boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+    // backgroundColor:"hwb(131 48% 0% / 0.267)"
+  };
+  
+  export const buttonStyle = {
+    margin: "1em 0.5em",
+    fontSize: "0.9em",
+    borderRadius: "20px",
+    border: "none",
+    fontFamily: "'Segoe UI', Tahoma, Geneva , Verdana, sans-serif",
+    backgroundColor: "#ffffff70",
+    color: "#242424",
+    fontSize: "15px",
+    fontWeight: "bold",
+    boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+    height: "38px",
+    width: "40px",
+    top: "-2px",
+    color: "#242424",
+    cursor: "pointer",
+    // paddingLeft:"12px"
+  };
+  
+  export const customButtonStyle = {
+   
+    width: "70px",
+    height: "30px",
+    fontSize: "14px",
+    backgroundColor: "#ffffff20",
+    // border: "1px sodlid #000",
+    // borderRadius: "25px"
+    // paddingLeft: "12px"
+  };
+  
+  export const taskStyles = {
+    fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
+    display: "flex",
+    backgroundColor: "#ffffff40",
+    borderRadius: "15px",
+    padding: "0em 1em",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    margin: "0.5em 0",
+    justifyContent: "space-between",
+    fontWeight: "bold",
+  };
